90c67205c89eb10a5df25c06985b5117a491cea8
Florencia <florencia.metelico94@gmail.com>
2023-04-17 22:08:54 -0300

    BaSP-W05-2023: Variables and Operators

---
+++ Week-05/exercises/variables-and-operators.js
@@ -0,0 +1,23 @@
+console.log("Variables and operators: A");
+// A. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+var numero=4;
+var numero2=8;
+var sumados=numero+numero2;
+
+console.log("The result is: "+sumados);
+
+console.log("Variables and operators: B");
+//B. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+var firstName = "Florencia";
+var lastName = "Metelico";
+var completeName=firstName+" "+lastName;
+
+console.log("My name is: "+completeName);
+
+console.log("Variables and operators: C");
+//Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+var word1="Inspector";
+var word2="Municipal";
+var totalWord=word1.length+word2.length;
+
+console.log("The total lenght is: "+totalWord);
