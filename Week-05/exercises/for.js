736ba778d87c4d5a1b6a8a20778b04125b9a3b83
Florencia <florencia.metelico94@gmail.com>
2023-04-18 23:15:35 -0300

    BaSP-W05-2023: Modify css and html of week 04

---
+++ Week-05/exercises/for.js
@@ -0,0 +1,11 @@
+console.log("----------------");
+
+console.log("For: A");
+//Crear un array que contenga 5 palabras y recorrer dicho 
+//array utilizando un bucle for de JavaScript para mostrar una 
+//alerta utilizando cada una de las palabras.
+
+var myList=["Planta", "Vela", "Lampara", "Maceta", "Mantel"];
+for (var i=0; i < myList.length; i++){
+    alert(myList[i]);
+}
